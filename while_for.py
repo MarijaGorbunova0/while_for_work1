072efc02e4e92b8dbf537abc027760a5120bddaa
opilane <opilane@HP-CZC2349HTD>
2024-01-19 11:39:40 +0200

    Add project files.

---
+++ while_for.py
@@ -0,0 +1,41 @@
+#1 for
+
+#r = 0
+#for i in range(15): #range(0,15,1)
+#    arv = float(input("Sisesta srv{0}.arv".format(i + 1)))
+#    if arv == int(arv):
+#        r += 1
+#print("täisarvude arv on"+ str(r))
+
+#2 while true 
+
+#r = 0
+#i = 0
+#while True:
+#    i+=1
+#    arv = float(input("Sisesta srv{0}.arv".format(i)))
+#    if arv == int(arv):
+#        r += 1  
+#        if i == 15: break
+#print("täisarvude arv on"+ str(r))
+
+# 1 wile tingimusega
+
+#r = 0
+#i = 0
+#while i<15:
+#    i+=1
+#    arv = float(input("Sisesta srv{0}.arv".format(i)))
+#    if arv == int(arv):
+#        r += 1  
+#print("täisarvude arv on"+ str(r))
+
+#3
+#otvet = 1
+#r = 0
+#for i in range(8):
+#    user = int(input("kirjutage arv{0}.arv".format(i+1)))
+#    if user > 0:
+#        otvet *=user
+#        r += 1
+#print("vastus on", otvet)
